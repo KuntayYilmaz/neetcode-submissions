7b0ed2018be73292e51f2d1cf2324409edaf4793
Kuntay Yılmaz <kuntayrylmz@gmail.com>
2026-06-27 10:42:39 +0300

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        int n = nums.size();
+        unordered_map<int,int> hashmap {};
+        for(int i = 0;i < n;i++)
+        {
+            if(hashmap.contains(target-nums[i]))
+            {
+                return {hashmap[target-nums[i]],i};
+            }
+            hashmap[nums[i]] = i;
+        }
+    }
+};
