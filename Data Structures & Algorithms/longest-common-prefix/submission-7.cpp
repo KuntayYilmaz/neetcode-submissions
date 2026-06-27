89502e44cb56dce522c3e2a0fbc89118387bc244
Kuntay Yılmaz <kuntayrylmz@gmail.com>
2026-06-27 11:29:26 +0300

    Add: longest-common-prefix - submission-7

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-7.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) 
+    {
+        int n = strs[0].size();
+        for(int i = 0;i < n;i++)
+        {
+            for(auto s : strs)
+            {
+                if(i == s.size() || strs[0][i] != s[i])
+                {
+                    return s.substr(0,i);
+                }
+            }
+        } 
+        return strs[0];   
+    }
+};
